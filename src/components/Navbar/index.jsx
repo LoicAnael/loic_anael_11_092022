2e63133895f4cddb9effa5b74ef4082cbb93c2ed
loic <loicanaelbdf@gmail.com>
2022-09-19 17:49:10 -0700

    banner done

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,21 +1,31 @@
-import { Link } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 import styled from 'styled-components'
 
-const NavbarContainer = styled.nav`
+const NavbarContainer = styled.ul`
   gap: 100px;
   height: 34px;
   display: flex;
-  justify-content: space-between;
-  align-items: center;
-  color: #ff6060;
-  text-decoration: none;
+  a {
+    text-decoration: none;
+  }
+  li {
+    list-style: none;
+    color: #ff6060;
+  }
+  .active li {
+    border-bottom: 2px solid #ff6060;
+  }
 `
 
 function Navbar() {
   return (
     <NavbarContainer>
-      <Link to="/">Accueil</Link>
-      <Link to="/a-propos">A propos</Link>
+      <NavLink to="/" exact activeClassName="active">
+        <li>Accueil</li>
+      </NavLink>
+      <NavLink to="/a-propos" exact activeClassName="active">
+        <li>A propos</li>
+      </NavLink>
     </NavbarContainer>
   )
 }
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,24 +1,19 @@
-import logo from '../../logo.svg'
-import '../../App.css'
+import Banniere from '../../components/Banniere'
+import styled from 'styled-components'
+
+const HomeContainer = styled.main`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding-left: 40px;
+  padding-right: 40px;
+`
 
 function Home() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <HomeContainer>
+      <Banniere />
+    </HomeContainer>
   )
 }
 
