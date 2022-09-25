5bd8a3e9066481a08d0bbaae00492f1783c7e1e7
loic <loicanaelbdf@gmail.com>
2022-09-25 16:06:17 -0700

    host details added

--- src/pages/Property/property.jsx
+++ src/pages/Property/property.jsx
@@ -8,10 +8,34 @@ function Details() {
   const ficheLogement = ListeLogements.find(
     (logement) => logement.id === getId.id
   )
-
+  // Tags
+  const TagsLogement = ficheLogement?.tags.map((tag, index) => {
+    return (
+      <span className="TagsLogement" key={index}>
+        {tag}
+      </span>
+    )
+  })
   return (
     <div className="LogementContainer">
       <Gallery images={ficheLogement?.pictures} />
+      <section className="LogementWrapper">
+        <div className="LogementInfos">
+          <span className="LogementTitre">{ficheLogement?.title} </span>
+          <span className="LogementLocation">{ficheLogement?.location}</span>
+          <span>{TagsLogement}</span>
+        </div>
+        <div className="HostWrapper">
+          <div className="HostInfo">
+            <span className="HostName">{ficheLogement?.host.name}</span>
+            <img
+              className="HostPicture"
+              src={ficheLogement?.host.picture}
+              alt="host pic"
+            />
+          </div>
+        </div>
+      </section>
     </div>
   )
 }
