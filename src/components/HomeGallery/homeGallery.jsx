24d1d9b0e56d4f553938d40d3c19b251a19a48e7
loic <loicanaelbdf@gmail.com>
2022-10-10 12:31:51 -0700

    fix

--- src/components/HomeGallery/homeGallery.jsx
+++ src/components/HomeGallery/homeGallery.jsx
@@ -2,7 +2,6 @@ import Card from '../Card/card.jsx'
 import { NavLink } from 'react-router-dom'
 import './homeGallery.css'
 import { useEffect, useState } from 'react'
-// import data from '../../data/logements.json'
 
 function Gallery() {
   const [logements, setLogement] = useState([])
@@ -11,9 +10,6 @@ function Gallery() {
       .then((rep) => rep.json())
       .then((data) => setLogement(data))
   }, [setLogement])
-  /*   useEffect(() => {
-    setLogement(data)
-  }, [setLogement]) */
 
   return (
     <section className="CardsContainer">
