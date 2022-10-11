484cf4f51a2120bc3975d9d4217e5bc6143dc9b9
loic <loicanaelbdf@gmail.com>
2022-10-10 23:36:24 -0700

    github repo publish on gh-pages

--- src/components/HomeGallery/homeGallery.jsx
+++ src/components/HomeGallery/homeGallery.jsx
@@ -2,13 +2,17 @@ import Card from '../Card/card.jsx'
 import { NavLink } from 'react-router-dom'
 import './homeGallery.css'
 import { useEffect, useState } from 'react'
+import data from '../../data/logements.json'
 
 function Gallery() {
   const [logements, setLogement] = useState([])
-  useEffect(() => {
+  /*   useEffect(() => {
     fetch('../../data/logements.json')
       .then((rep) => rep.json())
       .then((data) => setLogement(data))
+  }, [setLogement]) */
+  useEffect(() => {
+    setLogement(data)
   }, [setLogement])
 
   return (
